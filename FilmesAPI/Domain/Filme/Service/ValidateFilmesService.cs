bb6b8be8a891d3e295d56c69733afbb66f18b651
guiassys <guiassys@gmail.com>
2023-03-21 16:32:56 -0300

    Adiciona testes unitários para validar o objeto FilmeEntity

---
+++ FilmesAPI/Domain/Filme/Service/ValidateFilmesService.cs
@@ -0,0 +1,47 @@
+﻿
+using System.Data.SqlTypes;
+using FilmesApi.Domain.Filme.Models;
+
+
+namespace FilmesApi.Domain.Filme.Service
+{
+    public class ValidateFilmesService
+    {
+
+        public bool validateFilmeEntity(FilmeEntity filme) {
+            bool result = true;
+            if (filme == null)
+            {
+                result = false;
+            }
+
+            if (string.IsNullOrEmpty(filme.Titulo))
+            {
+                result = false;
+            }
+
+            if (string.IsNullOrEmpty(filme.Genero))
+            {
+                result = false;
+            }
+
+            if (null == filme.Duracao)
+            {
+                result = false;
+            }
+
+            if (filme.Duracao < 70)
+            {
+                result = false;
+            }
+
+            if (filme.Duracao > 600)
+            {
+                result = false;
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ FilmesApi.Test/FilmeTests/CreateFilmeTest.cs
@@ -0,0 +1,84 @@
+﻿
+using FilmesApi.Domain.Filme.Models;
+using FilmesApi.Domain.Filme.Service;
+
+namespace FilmesApi.Test.FilmeTests
+{
+    public class CreateFilmeTest
+    {
+        [Fact(DisplayName ="Verifica se o serviço de validação do objeto FilmeEntity retorna verdadeiro para objetos válidos")]
+        public void NaoDeveFalharAoValidarObjetoFilmeEntityValido() {
+            // Arrange
+            ValidateFilmesService validateFilmesService = new ValidateFilmesService();
+
+            FilmeEntity filme = new FilmeEntity();
+            filme.Titulo = "Título Teste";
+            filme.Genero = "Genero Teste";
+            filme.Duracao = 600;
+            // Act
+            var result = validateFilmesService.validateFilmeEntity(filme);
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact(DisplayName = "Deve retornar falso quando o Titulo for nulo")]
+        public void DeveRetornarFalsoQuandoFilmeNaoPossuiTitulo()
+        {
+            // Arrange
+            ValidateFilmesService validateFilmesService = new ValidateFilmesService();
+
+            FilmeEntity filme = new FilmeEntity();
+            filme.Genero = "Genero Teste";
+            filme.Duracao = 600;
+            // Act
+            var result = validateFilmesService.validateFilmeEntity(filme);
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact(DisplayName = "Deve retornar falso quando o Genero for nulo")]
+        public void DeveRetornarFalsoQuandoGeneroForNulo()
+        {
+            // Arrange
+            ValidateFilmesService validateFilmesService = new ValidateFilmesService();
+            FilmeEntity filme = new FilmeEntity();
+            filme.Titulo = "Titulo Teste";
+            filme.Duracao = 600;
+            // Act
+            var result = validateFilmesService.validateFilmeEntity(filme);
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact(DisplayName = "Deve retornar falso se a Duração for menor que 70")]
+        public void DeveRetornarFalsoQuandoDuracaoForMenor70()
+        {
+            // Arrange
+            ValidateFilmesService validateFilmesService = new ValidateFilmesService();
+            FilmeEntity filme = new FilmeEntity();
+            filme.Titulo = "Titulo Teste";
+            filme.Genero = "Genero Teste";
+            filme.Duracao = 69;
+            // Act
+            var result = validateFilmesService.validateFilmeEntity(filme);
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact(DisplayName = "Deve retornar falso se a Duração for mair que 600")]
+        public void DeveRetornarFalsoQuandoDuracaoForMaior600()
+        {
+            // Arrange
+            ValidateFilmesService validateFilmesService = new ValidateFilmesService();
+            FilmeEntity filme = new FilmeEntity();
+            filme.Titulo = "Titulo Teste";
+            filme.Genero = "Genero Teste";
+            filme.Duracao = 601;
+            // Act
+            var result = validateFilmesService.validateFilmeEntity(filme);
+            // Assert
+            Assert.False(result);
+        }
+
+    }
+}
