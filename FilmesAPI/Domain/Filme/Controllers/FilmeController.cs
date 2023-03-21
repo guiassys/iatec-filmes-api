1cfa50f82b0cb1df9fe20b60982abb62bcd42fdb
guiassys <guiassys@gmail.com>
2023-03-21 14:27:25 -0300

    Implementa camada de serviço

--- FilmesAPI/Domain/Filme/Controllers/FilmeController.cs
+++ FilmesAPI/Domain/Filme/Controllers/FilmeController.cs
@@ -2,6 +2,7 @@
 using FilmesApi.Data;
 using FilmesApi.Domain.Filme.Data;
 using FilmesApi.Domain.Filme.Models;
+using FilmesApi.Domain.Filme.Service;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,6 +13,7 @@ namespace FilmesApi.Domain.Filme.Controllers;
 public class FilmeController : ControllerBase
 {
 
+    private FilmesService filmesService;
     private FilmeRepository _context;
     private IMapper _mapper;
 
@@ -19,6 +21,7 @@ public class FilmeController : ControllerBase
     {
         _context = context;
         _mapper = mapper;
+        filmesService = new FilmesService(_context, _mapper);
     }
 
     /// <summary>
@@ -31,37 +34,71 @@ public class FilmeController : ControllerBase
     [ProducesResponseType(StatusCodes.Status201Created)]
     public IActionResult AdicionaFilme([FromBody] CreateFilmeDto filmeDto)
     {
-        FilmeEntity filme = _mapper.Map<FilmeEntity>(filmeDto);
-        _context.Filmes.Add(filme);
-        _context.SaveChanges();
+        FilmeEntity filme = filmesService.InsertFilme(filmeDto);
         return CreatedAtAction(nameof(RecuperaFilmePorId), new { id = filme.Id }, filme);
     }
 
+    /// <summary>
+    /// Recupera lista de filmes utilizando paginação
+    /// </summary>
+    /// <param name="skip">Informe o indice da paginação</param>
+    /// <param name="take">Informe a quantidade de registros</param>
+    /// <returns>IActionResult</returns>
+    /// <response code="200">Caso a busca seja feita com sucesso</response>
     [HttpGet]
     public IEnumerable<ReadFilmeDto> RecuperaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50)
     {
-        return _mapper.Map<List<ReadFilmeDto>>(_context.Filmes.Skip(skip).Take(take));
+        return filmesService.GetFilmes(skip, take);
     }
 
+    /// <summary>
+    /// Recupera filme aplicando filtro sobre o identificador do objeto
+    /// </summary>
+    /// <param name="id">Informe o identificador do objeto</param>
+    /// <returns>IActionResult</returns>
+    /// <response code="200">Caso a busca seja feita com sucesso</response>
     [HttpGet("{id}")]
     public IActionResult RecuperaFilmePorId(int id)
     {
-        var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+        var filme = filmesService.GetFilmeById(id);
         if (filme == null) return NotFound();
         var filmeDto = _mapper.Map<ReadFilmeDto>(filme);
         return Ok(filmeDto);
     }
 
+    /// <summary>
+    /// Atualiza filme aplicando filtro sobre o identificador do objeto
+    /// </summary>
+    /// <param name="id">Informe o identificador do objeto</param>
+    /// <returns>IActionResult</returns>
+    /// <response code="200">Caso a atualização seja feita com sucesso</response>
     [HttpPut("{id}")]
     public IActionResult AtualizaFilme(int id, [FromBody] UpdateFilmeDto filmeDto)
     {
-        var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
-        if (filme == null) return NotFound();
-        _mapper.Map(filmeDto, filme);
-        _context.SaveChanges();
+        filmesService.UpdateFilme(id, filmeDto);
         return NoContent();
     }
 
+    /// <summary>
+    /// Deleta filme aplicando filtro sobre o identificador do objeto
+    /// </summary>
+    /// <param name="id">Informe o identificador do objeto</param>
+    /// <returns>IActionResult</returns>
+    /// <response code="200">Caso a remoção seja feita com sucesso</response>
+    [HttpDelete("{id}")]
+    public IActionResult DeletaFilme(int id)
+    {
+        filmesService.DeleteFilme(id);
+        return NoContent();
+    }
+
+    /// <summary>
+    /// Atualiza filme parcialmente aplicando filtro sobre o identificador do objeto
+    /// </summary>
+    /// <param name="id">Informe o identificador do objeto</param>
+    /// <param name="patch">Objeto json representando os atributos que devem ser alterados</param>
+    /// <returns>IActionResult</returns>
+    /// <response code="200">Caso a atualização seja feita com sucesso</response>
     [HttpPatch("{id}")]
     public IActionResult AtualizaFilmeParcial(int id, JsonPatchDocument<UpdateFilmeDto> patch)
     {
@@ -77,17 +114,6 @@ public class FilmeController : ControllerBase
             return ValidationProblem(ModelState);
         }
         _mapper.Map(filmeParaAtualizar, filme);
-        _context.SaveChanges();
-        return NoContent();
-    }
-
-
-    [HttpDelete("{id}")]
-    public IActionResult DeletaFilme(int id)
-    {
-        var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
-        if (filme == null) return NotFound();
-        _context.Remove(filme);
         _context.SaveChanges();
         return NoContent();
     }
---
+++ FilmesAPI/Domain/Filme/Service/FilmesService.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+using FilmesApi.Data;
+using FilmesApi.Domain.Filme.Data;
+using FilmesApi.Domain.Filme.Models;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmesApi.Domain.Filme.Service
+{
+    public class FilmesService
+    {
+
+        private FilmeRepository _context;
+        private IMapper _mapper;
+
+        public FilmesService(FilmeRepository context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public FilmeEntity InsertFilme(CreateFilmeDto filmeDto)
+        {
+            FilmeEntity filme = _mapper.Map<FilmeEntity>(filmeDto);
+            _context.Filmes.Add(filme);
+            _context.SaveChanges();
+            return filme;
+        }
+
+        public IEnumerable<ReadFilmeDto> GetFilmes(int skip, int take)
+        {
+            return _mapper.Map<List<ReadFilmeDto>>(_context.Filmes.Skip(skip).Take(take));
+        }
+
+        public FilmeEntity GetFilmeById(int id)
+        {
+            FilmeEntity filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+            validateResultSearchFilmeById(id, filme);
+            return filme;
+        }
+
+        public FilmeEntity UpdateFilme(int id, UpdateFilmeDto filmeDto)
+        {
+            var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+            validateResultSearchFilmeById(id, filme);
+            _mapper.Map(filmeDto, filme);
+            _context.SaveChanges();
+            return filme;
+        }
+
+        public void DeleteFilme(int id)
+        {
+            var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+            validateResultSearchFilmeById(id, filme);
+            _context.Remove(filme);
+            _context.SaveChanges();
+        }
+
+        private void validateResultSearchFilmeById(int id, FilmeEntity filme) {
+            if (filme == null)
+            {
+                throw new Exception("Erro ao buscar o filme aplicando filtro pelo Id = "+ id);
+            }
+        }
+
+    }
+}
